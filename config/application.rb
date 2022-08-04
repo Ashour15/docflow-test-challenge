17712303cae52b1aa2b0fae3e4f07ac0958ec772
Ashour15 <mohamedashour15@hotmail.com>
2022-08-04 02:42:26 +0200

    feat: Setup app environment
    Setup rspec gem
    
    Config database

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,8 @@ module DocflowTestChallenge
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.autoload_paths += Dir["#{config.root}/app/services"]
+
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
